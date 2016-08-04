b720c3572cbcc53961ee23f5ab408f8f927f48a2
Hai <thanhhai.fu@gmail.com>
2016-08-04 15:02:55 +0700

    add checking External Storage
    Ensure that External Storage is available for read/write, otherwise using internal storage

--- app/src/main/java/com/postace/myruns1/MainActivity.java
+++ app/src/main/java/com/postace/myruns1/MainActivity.java
@@ -1,4 +1,4 @@
-package com.postace.myruns1;
+package com.postace.myruns1v2;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
@@ -10,11 +10,12 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -25,232 +26,249 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    // For photo
-    private static final int REQUEST_CODE_TAKE_PHOTO_FROM_CAMERA = 0;
-    private static final int REQUEST_CODE_CROP_PHOTO = 2;
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    // request code for sending intent
+    private static final int REQUEST_CODE_TAKE_PHOTO_FROM_CAMERA = 4;
+    private static final int REQUEST_CODE_CROP_IMAGE = 5;
 
     private static final String IMAGE_UNSPECIFIED = "image/*";
 
     private Uri mImageCaptureUri;
-    private ImageView mImageView;
-    boolean isTakenFromCamera;
+    private ImageView mUserAvatarImage;
+    private boolean isTakenFromCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mImageView = (ImageView) findViewById(R.id.user_photo);
-        // loading user data to screen from shared preferences
-        loadUserData();
-        // loading user profile photo
-        loadSnap();
-
+        mUserAvatarImage = (ImageView) findViewById(R.id.user_avatar);
+        // load user data and update UI
+        loadData();
     }
 
-    // ****************** onClick callbacks ****************** //
-    public void onSnapClicked(View v) {
-        // Construct intent with action MediaStore.ACTION_IMAGE_CAPTURE
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Construct temporary image path and name to save the taken photo
-        mImageCaptureUri = Uri.fromFile(new File(Environment
-                                .getExternalStorageDirectory(), "tmp_" +
-                                System.currentTimeMillis() + ".jpg"));
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
-        intent.putExtra("return-data", true);
-        try {
-            // start a camera capturing activity
-            startActivityForResult(intent, REQUEST_CODE_TAKE_PHOTO_FROM_CAMERA);
-        } catch (ActivityNotFoundException ae) {
-            ae.printStackTrace();
-        }
-    }
-
-    public void onSaveClicked(View v) {
-        // save all user information from the screen into a 'shared preferences'
-        saveUserData();
-        // notify user that data is saved
-        Toast.makeText(this, getString(R.string.toast_saved),
-                Toast.LENGTH_SHORT).show();
-    }
-
-    public void onCancelClicked(View v) {
-        // kill activity and exist application
-        finish();
-    }
-
+    // handle data when got result
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode != RESULT_OK) {
-            return;     // error, nothing to do
+            Toast.makeText(this, getString(R.string.toast_unknown_error),
+                    Toast.LENGTH_SHORT).show();
+            return;     // something error -> nothing to do
         }
 
         switch (requestCode) {
             case REQUEST_CODE_TAKE_PHOTO_FROM_CAMERA:
-                // cropping the image
+                // got picture, now crop it
                 cropImage();
                 isTakenFromCamera = true;
                 break;
-            case REQUEST_CODE_CROP_PHOTO:
-                // update image view after crop image
-                Bundle bundle = data.getExtras();
-                // set the picture image in UI
-                if (bundle != null) {
-                    mImageView.setImageBitmap((Bitmap) bundle.getParcelable("data"));
+            case REQUEST_CODE_CROP_IMAGE:
+                // Get photo data from intent
+                Bundle extras = data.getExtras();
+                // update user avatar in UI
+                if (extras != null) {
+                    Bitmap bm = extras.getParcelable("data");
+                    mUserAvatarImage.setImageBitmap(bm);
                 }
-                // Delete temporary image taken from camera after crop
+                // clear temporary image
                 if (isTakenFromCamera) {
                     File f = new File(mImageCaptureUri.getPath());
                     if (f.exists())
                         f.delete();
                 }
-                // save photo's profile
-                saveSnap();
-
                 break;
         }
     }
 
-    // ****************** private helper functions ****************** //
+    // Checking if external storage is available for read/write
+    public boolean isExternalStorageWritable() {
+        return Environment.MEDIA_MOUNTED.equals(
+                Environment.getExternalStorageState());
+    }
 
-    // load the user data from shared preferences if there is no data, make sure that
-    // we set it to something reasonable
-    private void loadUserData() {
-        // Get the shared preferences, create or update the activity pref object
-        String mKey = getString(R.string.pref_user_key);
-        SharedPreferences mPrefs = getSharedPreferences(mKey, MODE_PRIVATE);
-        // load the user name, update text_view
-        mKey = getString(R.string.pref_user_name_key);
-        String mUsername = mPrefs.getString(mKey, " ");
-        ((TextView) findViewById(R.id.text_name)).setText(mUsername);
-
-        // load the user email
-        mKey = getString(R.string.pref_user_email_key);
-        String mUserEmail = mPrefs.getString(mKey, " ");
-        ((TextView) findViewById(R.id.text_email)).setText(mUserEmail);
-
-        // load the user phone
-        mKey = getString(R.string.pref_user_phone_key);
-        String mUserPhone = mPrefs.getString(mKey, " ");
-        ((TextView) findViewById(R.id.text_phone_number)).setText(mUserPhone);
-
-        // load the user gender
-        mKey = getString(R.string.pref_user_gender_key);
-        // default gender -1 mean no radio button was set.
-        int genderValue = mPrefs.getInt(mKey, -1);
-        if (genderValue >= 0) {
-            // Find the button that should be checked
-            RadioButton radioBtn = (RadioButton) ((RadioGroup)
-                    findViewById(R.id.radioGender)).getChildAt(genderValue);
-            // Check the button
-            radioBtn.setChecked(true);
+    // ***************** button click callbacks ***************** //
+    public void onCaptureClicked(View v) {
+        // Construct and intent to take photo
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Construct an temporary image file and path for storing photo
+        // if external storage is not available, use internal storage
+        if (!isExternalStorageWritable()) {
+            mImageCaptureUri = Uri.fromFile(new File(
+                    getFilesDir(), "tmp_" + System.currentTimeMillis() + ".jpg"));
+        } else {    // otherwise, use external storage
+            mImageCaptureUri = Uri.fromFile(new File(
+                    Environment.getExternalStorageDirectory(),
+                    "tmp_" + System.currentTimeMillis() + ".jpg"));
         }
-
-        // load the user class
-        mKey = getString(R.string.pref_user_class_key);
-        String mUserClass = mPrefs.getString(mKey, " ");
-        ((TextView) findViewById(R.id.text_class)).setText(mUserClass);
-
-        // load the user major
-        mKey = getString(R.string.pref_user_major_key);
-        String mUserMajor = mPrefs.getString(mKey, " ");
-        ((TextView) findViewById(R.id.text_major)).setText(mUserMajor);
-    }
-
-    // saving user data in to shared preferences
-    private void saveUserData() {
-        // Getting the shared preferences editor
-        String mKey = getString(R.string.pref_user_key);
-        SharedPreferences mPrefs = getSharedPreferences(mKey, MODE_PRIVATE);
-
-        SharedPreferences.Editor mEditor = mPrefs.edit();
-        // clear previous data
-        mEditor.clear();
-
-        // save the user's information
-        // save user name
-        mKey = getString(R.string.pref_user_name_key);
-        String mUsername = ((TextView) findViewById(R.id.text_name)).getText().toString();
-        mEditor.putString(mKey, mUsername);
-        // save user email
-        mKey = getString(R.string.pref_user_email_key);
-        String mUserEmail = ((TextView) findViewById(R.id.text_email)).getText().toString();
-        mEditor.putString(mKey, mUserEmail);
-
-        // save user phone
-        mKey = getString(R.string.pref_user_phone_key);
-        String mUserPhone = ((TextView) findViewById(R.id.text_phone_number)).getText().toString();
-        mEditor.putString(mKey, mUserPhone);
-
-        // Read which radio index is checked
-        mKey = getString(R.string.pref_user_gender_key);
-        RadioGroup radioGender = (RadioGroup) findViewById(R.id.radioGender);
-        int genValue = radioGender.indexOfChild(findViewById(
-                        radioGender.getCheckedRadioButtonId()));
-        mEditor.putInt(mKey, genValue);
-
-        // save user class
-        mKey = getString(R.string.pref_user_class_key);
-        String mUserClass = ((TextView) findViewById(R.id.text_class)).getText().toString();
-        mEditor.putString(mKey, mUserClass);
-        // save user major
-        mKey = getString(R.string.pref_user_major_key);
-        String mUserMajor = ((TextView) findViewById(R.id.text_major)).getText().toString();
-        mEditor.putString(mKey, mUserMajor);
-
-        // commit all changes
-        mEditor.commit();
-    }
-
-    // save picture to internal storage
-    private void saveSnap() {
-        // Commit all changes into preferences file
-        // Save user profile photo into internal storage
-        mImageView.buildDrawingCache();
-        Bitmap bm = mImageView.getDrawingCache();
+        // set output for image
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
+        intent.putExtra("return-data", true);
         try {
-            FileOutputStream fos = openFileOutput(
-                                    getString(R.string.user_photo_file_name), MODE_PRIVATE);
-            bm.compress(Bitmap.CompressFormat.JPEG, 100, fos);
-            fos.flush();
-            fos.close();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
+            // start camera to take photo
+            startActivityForResult(intent, REQUEST_CODE_TAKE_PHOTO_FROM_CAMERA);
+        } catch (ActivityNotFoundException ae) {
+            Toast.makeText(this,
+                    getString(R.string.toast_msg_no_activity), Toast.LENGTH_SHORT).show();
         }
     }
 
-    // load user-picture profile
-    private void loadSnap() {
-        // load the photo from internal storage
-        try {
-            FileInputStream fis = openFileInput(getString(R.string.user_photo_file_name));
-            Bitmap bm = BitmapFactory.decodeStream(fis);
-            mImageView.setImageBitmap(bm);
-            fis.close();        // remember to close file
-        } catch (FileNotFoundException fe) {
-            // Default profile photo if no photo saved before
-            mImageView.setImageResource(R.drawable.sample);
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
+    public void onSaveClicked(View v) {
+        // saving user data
+        saveData();
+        // notify user that data is saved
+        Toast.makeText(this,
+                getString(R.string.toast_data_saved), Toast.LENGTH_SHORT).show();
     }
 
-    // Crop and resize image for the profile
+    public void onCancelClicked(View v) {
+        // exit application
+        finish();
+    }
+
+    // ******************* private helper functions ******************* //
+
+    // cropping image after taken photo from camera
     private void cropImage() {
-        // Use existing crop activity.
+        // Use existing activity for crop
         Intent intent = new Intent("com.android.camera.action.CROP");
         intent.setDataAndType(mImageCaptureUri, IMAGE_UNSPECIFIED);
 
         // Specify image size
-        intent.putExtra("outputX", 100);
-        intent.putExtra("outputY", 100);
+        intent.putExtra("outputX", 120);
+        intent.putExtra("outputY", 120);
 
-        // Specify aspect ratio, 1:1
+        // Specify image aspect ratio 1:1
         intent.putExtra("aspectX", 1);
         intent.putExtra("aspectY", 1);
         intent.putExtra("scale", true);
         intent.putExtra("return-data", true);
 
-        startActivityForResult(intent, REQUEST_CODE_CROP_PHOTO);
+        // start camera for cropping
+        try {
+            startActivityForResult(intent, REQUEST_CODE_CROP_IMAGE);
+        } catch (ActivityNotFoundException ae) {
+            Toast.makeText(this,
+                    getString(R.string.toast_msg_no_activity)
+                    , Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // saving user avatar to internal storage
+    private void savePhoto() {
+        mUserAvatarImage.buildDrawingCache();
+        Bitmap bm = mUserAvatarImage.getDrawingCache();
+        try {
+            FileOutputStream fos = openFileOutput(
+                    getString(R.string.user_avatar_photo_name), MODE_PRIVATE);
+            bm.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.flush();
+            fos.close();
+        } catch (IOException ioe) {
+            // cannot save photo
+            ioe.printStackTrace();
+        }
+    }
+
+    // loading photo saved and update in UI
+    private void loadPhoto() {
+        try {
+            FileInputStream fis = openFileInput(getString(R.string.user_avatar_photo_name));
+            Bitmap bm = BitmapFactory.decodeStream(fis);
+            mUserAvatarImage.setImageBitmap(bm);
+            fis.close();
+        } catch (IOException ioe) {
+            // if no photo saved before, load default photo
+            mUserAvatarImage.setImageResource(R.drawable.androidparty);
+        }
+    }
+
+    // ******************* Shared Preferences ******************* //
+
+    // load user data and update UI
+    private void loadData() {
+        // load photo avatar
+        loadPhoto();
+        // Get shared prefs object
+        String mKey = getString(R.string.pref_user_data_key);
+        SharedPreferences mPrefs = getSharedPreferences(mKey, MODE_PRIVATE);
+
+        // get name
+        mKey = getString(R.string.pref_user_name_key);
+        String mName = mPrefs.getString(mKey, " ");
+        ((EditText) findViewById(R.id.user_name_input)).setText(mName);
+
+        // get email
+        mKey = getString(R.string.pref_user_email_key);
+        String mEmail = mPrefs.getString(mKey, " ");
+        ((EditText) findViewById(R.id.user_email_input)).setText(mEmail);
+
+        // get phone number
+        mKey = getString(R.string.pref_user_phone_key);
+        String mPhone = mPrefs.getString(mKey, " ");
+        ((EditText) findViewById(R.id.user_phone_input)).setText(mPhone);
+
+        // get gender
+        mKey = getString(R.string.pref_user_gender_key);
+        int genId = mPrefs.getInt(mKey, -1);
+        if (genId >= 0) {
+            RadioButton radioBtn = (RadioButton)
+                    ((RadioGroup) findViewById(R.id.user_gender)).getChildAt(genId);
+            radioBtn.setChecked(true);
+        }
+
+        // get class
+        mKey = getString(R.string.pref_user_class_key);
+        String mClass = mPrefs.getString(mKey, " ");
+        ((EditText) findViewById(R.id.user_class_input)).setText(mClass);
+
+        // get major
+        mKey = getString(R.string.pref_user_major_key);
+        String mMajor = mPrefs.getString(mKey, " ");
+        ((EditText) findViewById(R.id.user_major_input)).setText(mMajor);
+    }
+
+    // storing user information into shared preferences
+    private void saveData() {
+        // saving user avatar
+        savePhoto();
+        // Getting shared prefs object
+        String mKey = getString(R.string.pref_user_data_key);
+        SharedPreferences mPrefs = getSharedPreferences(mKey, MODE_PRIVATE);
+        SharedPreferences.Editor mEditor = mPrefs.edit();
+        mEditor.clear();            // clear previous data
+
+        // save name
+        mKey = getString(R.string.pref_user_name_key);
+        String mName = ((EditText) findViewById(R.id.user_name_input)).getText().toString();
+        mEditor.putString(mKey, mName);
+
+        // save email
+        mKey = getString(R.string.pref_user_email_key);
+        String mEmail = ((EditText) findViewById(R.id.user_email_input)).getText().toString();
+        mEditor.putString(mKey, mEmail);
+
+        // save phone
+        mKey = getString(R.string.pref_user_phone_key);
+        String mPhone = ((EditText) findViewById(R.id.user_phone_input)).getText().toString();
+        mEditor.putString(mKey, mPhone);
+
+        // save gender
+        mKey = getString(R.string.pref_user_gender_key);
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.user_gender);
+        int genId = radioGroup.indexOfChild(findViewById(radioGroup.getCheckedRadioButtonId()));
+        mEditor.putInt(mKey, genId);
+
+        // save class
+        mKey = getString(R.string.pref_user_class_key);
+        String mclass = ((EditText) findViewById(R.id.user_class_input)).getText().toString();
+        mEditor.putString(mKey, mclass);
+
+        // save major
+        mKey = getString(R.string.pref_user_major_key);
+        String mMajor = ((EditText) findViewById(R.id.user_major_input)).getText().toString();
+        mEditor.putString(mKey, mMajor);
+
+        // commit all changes
+        mEditor.commit();
     }
 }
